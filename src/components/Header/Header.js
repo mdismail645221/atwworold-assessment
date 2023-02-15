de16450355af689664154d920962649e8949c13c
mdismail645221 <mdismail645221@gmail.com>
2023-02-15 14:53:26 +0600

    Navbar is completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import '../Header/Header.css'
+import '../Header/Header.css';
+import { FaSistrix } from "react-icons/fa";
 
 function Header() {
     return (
@@ -14,10 +15,13 @@ function Header() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse className='justify-content-evenly flex-grow-1' id="basic-navbar-nav">
                     <div className='navSearchBar'>
-                        <input className='w-full' type="text" placeholder='Search your favourie group in ATG' />
+                        <div className='navIconInput'>
+                            <FaSistrix />
+                            <input className='w-full' type="text" placeholder='Search your favourie group in ATG' />
+                        </div>
                     </div>
                     <Nav className='menu'>
-                        <NavDropdown className='fs-6' title={<><p className='d-inline'>Create Account <span>It's free</span></p></>} id="basic-nav-dropdown">
+                        <NavDropdown className='fs-6' title={<><p className='d-inline'>Create Account <span className='text-primary'>It's free</span></p></>} id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.4">
                                Login
                             </NavDropdown.Item>
